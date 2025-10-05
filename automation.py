01cc5d14401e20249df4d0e642ee04acf4539327
attila <attila.szarvas@gmail.com>
2025-10-05 11:55:12 +0200

    TV driven lights: make changes based on state prior to TV state change

--- automation.py
+++ automation.py
@@ -460,7 +460,7 @@ class Tv(Broadcaster):
         self._call_listeners()
 
     def _current_listener(self):
-        new_is_on = devices.ikea_smart_plug.current.get() > 0.2
+        new_is_on = devices.ikea_smart_plug.current.get() > 0.28
 
         if self._is_on is None:
             self._is_on = new_is_on
@@ -484,12 +484,22 @@ class Tv(Broadcaster):
 
 tv_state = Tv(devices.ikea_smart_plug.current)
 
+old_living_room_value = living_room.get()
+living_room_value_changed = False
 
 def tv_state_changed():
+    global old_living_room_value, living_room_value_changed
+
     if tv_state.get():
-        living_room.set(0.2)
+        old_living_room_value = living_room.get()
+
+        if (old_living_room_value > 0.25):
+            living_room.set(0.2)
+            living_room_value_changed = True
     else:
-        living_room.set(1.0)
+        if living_room_value_changed:
+            living_room.set(old_living_room_value)
+            living_room_value_changed = False
 
 
 tv_state.add_listener(tv_state_changed)
